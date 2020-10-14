95d179ff9e42bf692d474257a1870c10357ff0a9
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-10-15 00:03:43 +0530

    Missing icons

--- django_link/lib/main.dart
+++ django_link/lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+import 'dart:ui';
 import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -10,8 +13,6 @@ void main() {
 }
 
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -19,138 +20,143 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var data; // Json response is placed here
   bool pressed = false; // Show data
+  File jsonF; // File storage
+  Directory dir; // location of file
+  String filename = "Data.json"; // name of file
+  bool fileExists = false; // existance of file
+  Map<int, Color> colors =
+  {
+    50:Color.fromRGBO(136,14,79, .1),
+    100:Color.fromRGBO(136,14,79, .2),
+    200:Color.fromRGBO(136,14,79, .3),
+    300:Color.fromRGBO(136,14,79, .4),
+    400:Color.fromRGBO(136,14,79, .5),
+    500:Color.fromRGBO(136,14,79, .6),
+    600:Color.fromRGBO(136,14,79, .7),
+    700:Color.fromRGBO(136,14,79, .8),
+    800:Color.fromRGBO(136,14,79, .9),
+    900:Color.fromRGBO(136,14,79, 1),
+  };
 
-  MainAxisAlignment alignMain (String s){
-    switch(s){
-      case 'left':return MainAxisAlignment.start;
-      case 'right':return MainAxisAlignment.end;
-      default : return MainAxisAlignment.spaceEvenly;
-    }
+  @override
+  void initState() {
+    super.initState();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonF = new File(dir.path + "/" + filename);
+      fileExists = jsonF.existsSync();
+      if (fileExists) this.setState(() =>
+      data = json.decode(jsonF.readAsStringSync()));
+    });
   }
 
-  CrossAxisAlignment alignCross (String s){
-    switch(s){
-      case 'left':return CrossAxisAlignment.start;
-      case 'right':return CrossAxisAlignment.end;
-      default : return CrossAxisAlignment.stretch;
-    }
+  // Creating json file
+  void createFile(Map<String, dynamic> content, Directory dir, String fileName) {
+      print("Creating file!");
+      File file = new File(dir.path + "/" + fileName);
+      file.create().then((value) => fileExists = true);
+      file.writeAsStringSync(json.encode(content));
   }
 
-  // Display list of Icons
-  Widget GetIcon(List<dynamic> strings, String color){
-    String colored = color.replaceAll('#', '0xff'); // Hex code of colour
-    List<Widget> list = List<Widget>();
-
-    for(var i = 0; i < strings.length; i++){
-      list.add(Container(
-        margin: const EdgeInsets.all(15.0),
-        padding: const EdgeInsets.all(3.0),
-        child:Icon(
-          FontAwesomeIcons.icons,
-        ),
-      ),
-      );
+  // Writing to json file
+  void writeFile(Map<String, dynamic> content){
+    print("Writing File");
+      if (fileExists) {
+        print("File exists");
+        var jsonFileContent = json.decode(jsonF.readAsStringSync());
+        jsonFileContent.addAll(content);
+        jsonF.writeAsStringSync(json.encode(jsonFileContent));
+      } else {
+        print("File does not exist!");
+        createFile(content, dir, filename);
+      }
+      this.setState(() => data = json.decode(jsonF.readAsStringSync()));
     }
-    return Container(
-      color: Color(int.parse(colored)),
-      child:Column(
-        children:<Widget>[
-          Row(
-            mainAxisAlignment: alignMain(data['pos']),
-            children:list ,
-          ),
-        ],
-      ),
-    );
+
+    // Colour from hex to RGB
+  MaterialColor getColor(String color){
+      int colored = int.parse(color.replaceAll('#', '0xff')); // Hex code for colour
+      MaterialColor colorCustom = MaterialColor(colored, colors);
+    return colorCustom;
   }
 
-  // Display list of Titles or Texts
-  Widget GetText(List<dynamic> strings, String color){
-    String colored = color.replaceAll('#', '0xff'); // Hex code for colour
-    List<Widget> list = List<Widget>();
-    for(var i = 0; i < strings.length; i++){
-      list.add(Container(
-        margin: const EdgeInsets.all(15.0),
-        padding: const EdgeInsets.all(3.0),
-          child:Text(
-              strings[i]
-          ),
-        ),
-      );
+  // Title conversion
+  String setTitle(){
+    String titles = "";
+    for(var i = 0;i<data['title'].length;i++){
+      titles += data['title'][i];
+      if (i!=data['title'].length - 1) titles += " / ";
     }
-    return Container(
-      color: Color(int.parse(colored)),
-      child:Column(
-        children:<Widget>[
-        Row(
-          mainAxisAlignment: alignMain(data['pos']),
-          children:list ,
-        ),
-      ],
-    ),
-    );
+    return titles;
   }
-
+  
   // Get Json Response
-
-  Future<String> getData() async{
-  var response = await http.get(
-    Uri.encodeFull('http://10.0.2.2:8000/header/api/headers/1/'),
-    headers: {
-      "key":"^843@9fn#nk3tr#ril53u_3fv+3s(vptmmj(d7x40y^ins((gl",
-      "Accept":"application/json",
-    }
-  );
-  var get_data = json.decode(response.body);
-  data = get_data;
-  return "";
+  void getData() async{
+    var response = await http.get(
+      Uri.encodeFull('http://10.0.2.2:8000/header/api/headers/1/'),
+      headers: {
+        "key":"^843@9fn#nk3tr#ril53u_3fv+3s(vptmmj(d7x40y^ins((gl",
+        "Accept":"application/json",
+      }
+    );
+    writeFile(json.decode(response.body));
   }
 
+  List<PopupMenuEntry> popUp(){
+    List<PopupMenuEntry> lis = [];
+    for(var i = 0;i<data['text'].length;i++){
+      lis.add(PopupMenuItem(
+        value: i,
+        child: Text(data['text'][i]),
+      )
+      );
+    }
+    return lis;
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: data != null ? getColor(data['colour']): Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello"),
-        ),
-        body:Column(
-            children:<Widget>[
-              data != null && pressed ?
-                  Column(
-                    crossAxisAlignment: alignCross(data['pos']),
-                    children: [
-                    GetText(data['title'], data['colour']),
-                    GetText(data['text'],data['colour']),
-                    GetIcon(data['icon'],data['colour']),
-                    Text("Colour = "  + data["colour"]),
-                    Text("Menu Direction = " + data["pos"]),
-                  ],
-                  )
-               : new Center(child: new CircularProgressIndicator()),
-                  Container(
-                      child: RaisedButton(
-                        child: Text("Get Data"),
-                        onPressed: getData,
-                      )
-                  ),
-              Container(
-                  child: RaisedButton(
-                    child: Text("Show data"),
-                    onPressed: (){
-                      setState(() {
-                        pressed = !pressed;
-                      });
-                    },
-                  )
-              ),
-                ],
-              ),
+          title: data != null ? Text(setTitle()):Text("NO DATA IN JSON"),
+          actions: <Widget>[
+            PopupMenuButton(
+              onSelected: (value) {
+                setState((){
+                  print(value);
+                });
+              },
+              itemBuilder: (BuildContext context) {
+                return popUp();
+              },
             ),
+          ],
+
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: RaisedButton(
+                  child: Text('Get new data'),
+                  onPressed: () {
+                    setState(() {
+                      getData();
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
